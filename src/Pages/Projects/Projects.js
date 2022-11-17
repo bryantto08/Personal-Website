735297c156b5c10fb7cef857dbc18bdbf1caac2d
bryantto08 <bryantto08@gmail.com>
2022-11-16 22:02:23 -0500

    css animation pt1 for projects page

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-import logo from "./ProjectImages/amogus.jpg";
+import logo from "./ProjectImages/discord-ss.png";
 import "./Projects.css";
 function Projects() {
+    const [DiscordBot, setDiscordBot] = useState(false);
     return(
         <div>
             <div>
@@ -12,11 +13,24 @@ function Projects() {
                 <h2>My Projects: </h2>
                 <div className="project-options">
                     <button className="project-button">ESPN Scraper</button>
-                    <button className="project-button">Discord Bot</button>
+                    <button className="project-button" onClick={(e) => {setDiscordBot(true)}}>Discord Bot</button>
                 </div>
-                <div>
-                    <img className="project-img" src={logo} alt="logo"></img>
+                {DiscordBot && (
+                <div className="project-content">
+                <div className="project-text">
+                    <h3>Discord Bot</h3>
+                    <h5>Fetches information from the Riot Games API and displays it to the user via embeds.</h5>
+                    <p>Retrieves League of Legends information such as:</p>
+                    <ul>
+                        <li>Player's Match History</li>
+                        <li>Player's Statistics</li>
+                        <li>Player's Game Ranks</li>
+                        <li>etc.</li>
+                    </ul>
                 </div>
+                <img className="project-img" src={logo} alt="logo"></img>
+            </div>  
+                )}
             </div>
 
         </div>
